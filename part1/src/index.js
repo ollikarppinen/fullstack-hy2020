05c595ceb936b4dc6429b8de70e95c62805c1d84
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-19 21:52:59 +0200

    Adds solution for 1.3.-1.5.

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,31 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const parts = [
-    { name: part1, exerciseCount: exercises1 },
-    { name: part2, exerciseCount: exercises2 },
-    { name: part3, exerciseCount: exercises3 }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
 const Header = ({ course }) => <h1>{course}</h1>
 const Content = ({ parts }) => <>{parts.map(Part)}</>
-const Part = ({ name, exerciseCount }) => <p>{name} {exerciseCount}</p>
-const Total = ({ parts }) => <p>Number of exercises {parts.reduce((sum, { exerciseCount }) => sum + exerciseCount, 0)}</p>
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+const Total = ({ parts }) => <p>Number of exercises {parts.reduce((sum, { exercises }) => sum + exercises, 0)}</p>
 
 ReactDOM.render(<App />, document.getElementById('root'))
