f633fd1ca01fb8e612386127e8697851107f1a13
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-19 22:05:56 +0200

    Adds 1.6 solution

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>bad</button>
+      <button onClick={() => setBad(bad + 1)}>netral</button>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
