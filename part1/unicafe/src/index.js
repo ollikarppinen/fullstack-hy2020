4e4486f65444422ef6a83d39748d794da67d101b
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-19 22:25:53 +0200

    Adds 1.10 solution

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,39 +2,42 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} label={'good'} />
+      <Button onClick={() => setNeutral(neutral + 1)} label={'neutral'} />
+      <Button onClick={() => setBad(bad + 1)} label={'bad'} />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
+const Button = ({ onClick, label }) => <button onClick={onClick}>{label}</button>
+
 const Statistics = ({ good, neutral, bad }) => {
   const voteCount = good + neutral + bad
 
-  return <>
+  return <div>
     <h1>statistics</h1>
     {voteCount ? (
       <>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {voteCount}</div>
-        <div>average {(good - bad) / voteCount}</div>
-        <div>positive {good / voteCount} %</div>
+        <Statistic text='good' value={good}/>
+        <Statistic text='neutral' value={neutral}/>
+        <Statistic text='bad' value={bad}/>
+        <Statistic text='all' value={voteCount}/>
+        <Statistic text='average' value={(good - bad) / voteCount}/>
+        <Statistic text='positive' value={`${good / voteCount} %`}/>
       </>
     ): 'No feedback given'}
     
-  </>
+  </div>
 }
+
+const Statistic = ({ text, value }) => <div>{text} {value}</div>
 
 ReactDOM.render(<App />,
   document.getElementById('root')
