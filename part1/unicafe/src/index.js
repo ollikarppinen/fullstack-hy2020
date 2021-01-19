0877d628a9867ad35f4a7b4fdaef8e9a8a7514fd
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-19 22:11:08 +0200

    Adds 1.7 solution

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,17 +6,20 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const voteCount = good + neutral + bad
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>bad</button>
-      <button onClick={() => setBad(bad + 1)}>netral</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {voteCount}</div>
+      <div>average {(good - bad) / voteCount}</div>
+      <div>positive {good / voteCount} %</div>
     </div>
   )
 }
