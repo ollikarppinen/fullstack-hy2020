56ec754a7621d454c985ba4baad5219cf79af30c
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-22 18:25:16 +0200

    Adds 2.4 solution

--- part2/course/src/index.js
+++ part2/course/src/index.js
@@ -10,7 +10,7 @@ const Header = ({ course: { name } }) => {
 const Total = ({ course: { parts = [] } }) => {
   const sum = parts.reduce((sum, { exercises = 0 }) => sum + exercises, 0)
   return (
-    <p>Number of exercises {sum}</p>
+    <p><strong>Number of exercises {sum}</strong></p>
   )
 }
 
@@ -30,36 +30,61 @@ const Content = ({ course: { parts = [] } }) => {
   )
 }
 
-const Course = ({ course = {} }) => <div>
+const Course = course => <div>
   <Header course={course} />
   <Content course={course} />
   <Total course={course} />
 </div>
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return <div>
+    {courses.map(Course)}
+  </div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
