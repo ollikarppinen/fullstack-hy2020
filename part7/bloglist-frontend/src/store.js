c19cf035abf9077f48f79807c7d641bc394e01b4
Olli Karppinen <olli.karppinen@relex.fi>
2021-03-20 13:02:44 +0200

    Adds redux thunk and logger

--- part7/bloglist-frontend/src/store.js
+++ part7/bloglist-frontend/src/store.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers } from "redux";
+import { applyMiddleware, createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import reduxThunk from "redux-thunk";
+import reduxLogger from "redux-logger";
 
-// import noteReducer from "./reducers/noteReducer";
-// import filterReducer from "./reducers/filterReducer";
+const middleware = [reduxLogger, reduxThunk];
 
 const reducer = combineReducers({});
 
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
