3d6028c7d6fa61d134370147473c0b9d1801a45a
Olli Karppinen <olli.karppinen@relex.fi>
2021-03-24 00:08:29 +0200

    Adds dumb stuff

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
@@ -17,37 +17,55 @@ import Navbar from "./components/Navbar";
 const App = () => {
   const currentUser = useSelector(({ currentUser }) => currentUser);
 
+  const [filterValue, setFilterValue] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => setFilterValue(filterValue + 5), 100);
+  }, [filterValue]);
+
+  const hueRotateValue = filterValue % 10000;
+  const invertValue = Math.floor((filterValue / 100) % 2)
+    ? 100 - (filterValue % 100)
+    : filterValue % 100;
+
+  const style = {
+    filter: `hue-rotate(${hueRotateValue}deg) invert(${invertValue}%)`,
+    height: "100%",
+  };
+
   if (!currentUser)
     return (
-      <Container>
+      <Container style={style}>
         <LoginForm />
       </Container>
     );
 
   return (
-    <Router>
-      <Navbar />
-      <Container>
-        <Notification />
-        <Switch>
-          <Route exact path={["/", "/blogs"]}>
-            <BlogList />
-          </Route>
-          <Route path="/blogs/:id">
-            <BlogDetails />
-          </Route>
-          <Route path="/users/:id">
-            <UserDetails />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-      </Container>
-    </Router>
+    <div style={style}>
+      <Router>
+        <Navbar />
+        <Container>
+          <Notification />
+          <Switch>
+            <Route exact path={["/", "/blogs"]}>
+              <BlogList />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="/users/:id">
+              <UserDetails />
+            </Route>
+            <Route path="/users">
+              <UserList />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    </div>
   );
 };
 
--- part7/bloglist-frontend/src/index.js
+++ part7/bloglist-frontend/src/index.js
@@ -5,6 +5,12 @@ import { Provider } from "react-redux";
 
 import store from "./store";
 
+const animateBg = (i) => {
+  document.body.style.backgroundColor = "hsl(" + i + ", 100%, 50%)";
+  setTimeout(() => animateBg(++i), 100);
+};
+animateBg(0);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
