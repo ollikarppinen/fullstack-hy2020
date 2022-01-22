4168875e34d63bc0675a3d9ea47eacaf83cd4325
Olli Karppinen <olli.karppinen@relex.fi>
2022-01-22 23:04:48 +0200

    Adds 8.8 solution

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,33 +1,52 @@
-import React, { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import React, { useState, useEffect } from "react";
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+  }),
+});
+
+const query = gql`
+  query AllAuthors {
+    allAuthors {
+      bookCount
+      name
+      born
+    }
+  }
+`;
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState("authors");
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    client.query({ query }).then((response) => {
+      setAuthors(response.data.allAuthors);
+    });
+  });
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
       </div>
 
-      <Authors
-        show={page === 'authors'}
-      />
+      <Authors authors={authors} show={page === "authors"} />
 
-      <Books
-        show={page === 'books'}
-      />
+      <Books show={page === "books"} />
 
-      <NewBook
-        show={page === 'add'}
-      />
-
+      <NewBook show={page === "add"} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
