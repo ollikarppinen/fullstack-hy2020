e59ee856067dfc0e59faa9ca5510f2fa1bb10c2e
Olli Karppinen <olli.karppinen@relex.fi>
2022-01-24 23:29:11 +0200

    Adds user creation and login

--- part8/graphql-server/library-backend.js
+++ part8/graphql-server/library-backend.js
@@ -1,12 +1,16 @@
 const { ApolloServer, gql, UserInputError } = require("apollo-server");
 
+const jwt = require("jsonwebtoken");
+
 const mongoose = require("mongoose");
 const Book = require("./models/book");
 const Author = require("./models/author");
+const User = require("./models/user");
 
 require("dotenv").config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 console.log("connecting to", MONGODB_URI);
 
@@ -35,11 +39,22 @@ const typeDefs = gql`
     bookCount: Int
   }
 
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
     bookCount: Int!
     authorCount: Int!
+    me: User
   }
 
   type Mutation {
@@ -50,6 +65,8 @@ const typeDefs = gql`
       author: String!
     ): Book
     editAuthor(name: String!, setBornTo: Int!): Author
+    createUser(username: String!, favoriteGenre: String!): User
+    login(username: String!, password: String!): Token
   }
 `;
 
@@ -125,6 +142,33 @@ const resolvers = {
           invalidArgs: args,
         });
       }
+    },
+    createUser: async (root, args) => {
+      const user = new User({ ...args });
+
+      try {
+        await user.save();
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        });
+      }
+
+      return user;
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== "secret") {
+        throw new UserInputError("wrong credentials");
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
     },
   },
 };
