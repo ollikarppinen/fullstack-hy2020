04b0911c5b70ed991f4203cdf48bb57672e9be4f
Olli Karppinen <olli.karppinen@relex.fi>
2022-01-30 16:39:43 +0200

    Adds 9.7 solution

--- part9/foo/exerciseCalculator.ts
+++ part9/foo/exerciseCalculator.ts
@@ -20,7 +20,10 @@ interface ResultoObject {
   average: number;
 }
 
-const calculateExercises = (hours: number[], target: number): ResultoObject => {
+export const calculateExercises = (
+  hours: number[],
+  target: number
+): ResultoObject => {
   const periodLength = hours.length;
   const trainingDays = hours.filter((i) => i > 0).length;
   const average = hours.reduce((sum, i) => sum + i, 0) / periodLength;
@@ -44,15 +47,15 @@ const calculateExercises = (hours: number[], target: number): ResultoObject => {
   };
 };
 
-try {
-  const [_cmd1, _cmd2, targetArg, ...hoursArg] = process.argv;
-  const target: number = Number(targetArg);
-  const hours: number[] = hoursArg.map((i) => Number(i));
-  console.log(calculateExercises(hours, target));
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong.";
-  if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
-  }
-  console.log(errorMessage);
-}
+// try {
+//   const [_cmd1, _cmd2, targetArg, ...hoursArg] = process.argv;
+//   const target: number = Number(targetArg);
+//   const hours: number[] = hoursArg.map((i) => Number(i));
+//   console.log(calculateExercises(hours, target));
+// } catch (error: unknown) {
+//   let errorMessage = "Something went wrong.";
+//   if (error instanceof Error) {
+//     errorMessage += " Error: " + error.message;
+//   }
+//   console.log(errorMessage);
+// }
--- part9/foo/index.ts
+++ part9/foo/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 const app = express();
+
+var jsonParser = bodyParser.json();
 
 app.get("/hello", (req, res) => {
   console.log("res.query", req.query);
@@ -25,6 +29,39 @@ app.get("/bmi", (req, res) => {
   }
   const bmi = calculateBmi(height, weight);
   return res.send({ weight, height, bmi });
+});
+
+app.post("/exercises", jsonParser, (req, res) => {
+  let hours, target;
+
+  if (
+    !req.body.hasOwnProperty("target") ||
+    !req.body.hasOwnProperty("daily_exercises")
+  ) {
+    res.status(400);
+    return res.send({ error: "parameters missing" });
+  }
+  try {
+    hours = req.body.daily_exercises.map((i: string) => parseInt(i));
+    target = parseInt(req.body.target);
+
+    if (
+      isNaN(target) ||
+      hours.reduce((bool: boolean, el: number) => bool || isNaN(el), false)
+    ) {
+      throw "Parameter is not a number!";
+    }
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    console.log(errorMessage);
+    res.status(400);
+    return res.send({ error: "malformatted parameters" });
+  }
+  const result = calculateExercises(hours, target);
+  return res.send(result);
 });
 
 const PORT = 3003;
