ac7cd9f8f49a7f38a37c8505a55ef31433628d60
Olli Karppinen <olli.karppinen@relex.fi>
2022-01-30 18:09:05 +0200

    Adds /api/ping endpoint

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
   console.log("someone pinged here");
   res.send("pong");
 });
