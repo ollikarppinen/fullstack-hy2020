b1fbf7dc776d1a44eaa4e21298c0553749d51c65
Olli Karppinen <olli.karppinen@relex.fi>
2022-01-30 22:58:45 +0200

    Adds 9.10 solution

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import diagnoseRouter from "./src/routes/diagnoses";
+
 const app = express();
 app.use(express.json());
 
@@ -9,6 +11,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor-backend/src/routes/diagnoses.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import diagnoseService from "../services/diagnoseService";
+
+const router = express.Router();
+
+router.get("/", (_req, res) => {
+  const diagnoses = diagnoseService.getEntries();
+  res.set("Access-Control-Allow-Origin", "*");
+  console.log("someone pinged here");
+  res.send(diagnoses);
+});
+
+export default router;
