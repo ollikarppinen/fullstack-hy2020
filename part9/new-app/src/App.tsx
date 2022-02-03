48bd87d85e73bc06021b4fbcca9664d582ad5ffa
Olli Karppinen <olli.karppinen@relex.fi>
2022-02-03 22:01:47 +0200

    Adds 9.15 solution

--- part9/new-app/src/App.tsx
+++ part9/new-app/src/App.tsx
@@ -4,22 +4,86 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Total from "./components/Total";
 
+export enum CourseType {
+  Normal = "normal",
+  Submission = "submission",
+  GroupProject = "groupProject",
+  Special = "special",
+}
+
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: CourseType;
+}
+
+interface CoursePartBaseWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartBaseWithDescription {
+  type: CourseType.Normal;
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: CourseType.GroupProject;
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBaseWithDescription {
+  type: CourseType.Submission;
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartBaseWithDescription {
+  type: CourseType.Special;
+  requirements: string[];
+}
+
+export type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseSpecialPart;
+
+// this is the new coursePart variable
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is the leisured course part",
+    type: CourseType.Normal,
+  },
+  {
+    name: "Advanced",
+    exerciseCount: 7,
+    description: "This is the harded course part",
+    type: CourseType.Normal,
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: CourseType.GroupProject,
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+    type: CourseType.Submission,
+  },
+  {
+    name: "Backend development",
+    exerciseCount: 21,
+    description: "Typing the backend",
+    requirements: ["nodejs", "jest"],
+    type: CourseType.Special,
+  },
+];
+
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7,
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14,
-    },
-  ];
 
   const exerciseCountSum = courseParts.reduce(
     (carry, part) => carry + part.exerciseCount,
--- part9/new-app/src/components/Content.tsx
+++ part9/new-app/src/components/Content.tsx
@@ -1,22 +1,32 @@
 import React from "react";
 
-type CoursePart = {
-  name: string;
-  exerciseCount: number;
-};
+import { CoursePart, CourseType } from "../App";
 
 interface ContentProps {
   courseParts: CoursePart[];
 }
 
 const Content = ({ courseParts }: ContentProps) => {
-  return <>{courseParts.map(CoursePart)}</>;
+  return <>{courseParts.map(Part)}</>;
 };
 
-const CoursePart = ({ name, exerciseCount }: CoursePart) => (
-  <p>
-    {name} {exerciseCount}
-  </p>
-);
+const Part = (coursePart: CoursePart) => {
+  let str;
+  switch (coursePart.type) {
+    case CourseType.GroupProject:
+      str = `${coursePart.name} ${coursePart.exerciseCount} ${coursePart.type} ${coursePart.groupProjectCount}`;
+      break;
+    case CourseType.Normal:
+      str = `${coursePart.name} ${coursePart.exerciseCount} ${coursePart.type}`;
+      break;
+    case CourseType.Submission:
+      str = `${coursePart.name} ${coursePart.exerciseCount} ${coursePart.type} ${coursePart.description} ${coursePart.exerciseSubmissionLink}`;
+      break;
+    case CourseType.Special:
+      str = `${coursePart.name} ${coursePart.exerciseCount} ${coursePart.type} ${coursePart.description} ${coursePart.requirements}`;
+      break;
+  }
+  return <p>{str}</p>;
+};
 
 export default Content;
