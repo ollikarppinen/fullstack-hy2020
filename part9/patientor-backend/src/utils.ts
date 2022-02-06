cb8046e8a4205f12e14fcffe3d200e291a06296b
Olli Karppinen <olli.karppinen@relex.fi>
2022-02-06 20:47:29 +0200

    Adds task 9.19 solution

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,11 +1,19 @@
-import { NewPatient, Gender } from "./types";
+import {
+  NewPatient,
+  Gender,
+  Entry,
+  EntryType,
+  SickLeave,
+  Discharge,
+} from "./types";
 
-type Fields = {
+type PatientFields = {
   name: unknown;
   dateOfBirth: unknown;
   ssn: unknown;
   gender: unknown;
   occupation: unknown;
+  entries?: unknown;
 };
 
 const toNewPatient = ({
@@ -14,17 +22,105 @@ const toNewPatient = ({
   ssn,
   gender,
   occupation,
-}: Fields): NewPatient => {
+  entries,
+}: PatientFields): NewPatient => {
   const newPatient: NewPatient = {
     name: parseName(name),
-    dateOfBirth: parseDateOfBirth(dateOfBirth),
+    dateOfBirth: parseDate(dateOfBirth),
     ssn: parseSsn(ssn),
     gender: parseGender(gender),
     occupation: parseOccupation(occupation),
-    entries: [],
+    entries: parseEntries(entries),
   };
 
   return newPatient;
+};
+
+type EntryFields = {
+  id: unknown;
+  date: unknown;
+  type: unknown;
+  specialist: unknown;
+  diagnosisCodes: unknown;
+  description: unknown;
+  employerName?: unknown;
+  sickLeave: unknown;
+  discharge: unknown;
+};
+
+const parseEntries = (entries: unknown): Entry[] => {
+  if (!entries) return [];
+  if (!Array.isArray(entries)) throw new Error("Incorrect or missing entries");
+  return entries.map(parseEntry);
+};
+
+const parseEntry = ({
+  id,
+  date,
+  type,
+  specialist,
+  diagnosisCodes,
+  description,
+  employerName,
+  sickLeave,
+  discharge,
+}: EntryFields): Entry => {
+  const entryType = parseEntryType(type);
+
+  switch (entryType) {
+    case EntryType.Hospital:
+      return {
+        id: parseString(id),
+        date: parseDate(date),
+        type: EntryType.Hospital,
+        specialist: parseString(specialist),
+        diagnosisCodes: parseStringArray(diagnosisCodes),
+        description: parseString(description),
+        discharge: parseDischarge(discharge),
+      };
+    case EntryType.OccupationalHealthcare:
+      return {
+        id: parseString(id),
+        date: parseDate(date),
+        type: EntryType.Hospital,
+        specialist: parseString(specialist),
+        diagnosisCodes: parseStringArray(diagnosisCodes),
+        description: parseString(description),
+        employerName: parseString(employerName),
+        sickLeave: parseSickLeave(sickLeave),
+      };
+    default:
+      throw new Error("Incorrect or missing entry type");
+  }
+};
+
+const parseSickLeave = ({ startDate, endDate }: any): SickLeave => {
+  return {
+    startDate: parseDate(startDate),
+    endDate: parseDate(endDate),
+  };
+};
+
+const parseDischarge = ({ date, criteria }: any): Discharge => {
+  return {
+    date: parseDate(date),
+    criteria: parseString(criteria),
+  };
+};
+
+const parseString = (string: unknown): string => {
+  if (!string || !isString(string)) {
+    throw new Error("Incorrect or missing string");
+  }
+
+  return string;
+};
+
+const parseStringArray = (array: unknown): string[] => {
+  if (!array || !Array.isArray(array))
+    throw new Error("Incorrect or missing string array");
+
+  return array.map(parseString);
 };
 
 const parseName = (name: unknown): string => {
@@ -35,9 +131,9 @@ const parseName = (name: unknown): string => {
   return name;
 };
 
-const parseDateOfBirth = (date: unknown): string => {
+const parseDate = (date: unknown): string => {
   if (!date || !isString(date) || !isDate(date)) {
-    throw new Error("Incorrect or missing date of birth: " + date);
+    throw new Error("Incorrect or missing date: " + date);
   }
   return date;
 };
@@ -56,6 +152,16 @@ const parseGender = (gender: unknown): Gender => {
   }
   return gender;
 };
+
+const parseEntryType = (type: unknown): EntryType => {
+  if (!type || !isEntryType(type)) {
+    throw new Error("Incorrect or missing type: " + type);
+  }
+  return type;
+};
+
+const isEntryType = (type: any): type is EntryType =>
+  Object.values(EntryType).includes(type);
 
 const parseOccupation = (occupation: unknown): string => {
   if (!occupation || !isString(occupation)) {
