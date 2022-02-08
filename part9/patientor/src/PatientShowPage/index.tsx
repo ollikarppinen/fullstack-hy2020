6f2683f60b525aa81f729240ed57f498bf62d943
Olli Karppinen <olli.karppinen@relex.fi>
2022-02-08 21:51:14 +0200

    Adds task 9.22 solution

--- part9/patientor/src/PatientShowPage/index.tsx
+++ part9/patientor/src/PatientShowPage/index.tsx
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 import { apiBaseUrl } from "../constants";
-import { Container, Icon, SemanticICONS } from "semantic-ui-react";
+import { Container, Icon, SemanticICONS, Card, Feed } from "semantic-ui-react";
 import { useParams } from "react-router-dom";
 import { useStateValue } from "../state";
-import { Gender, Patient } from "../types";
+import { Gender, Patient, EntryType, Discharge, SickLeave } from "../types";
 import { addPatient } from "../state/reducer";
 
 const PatientShowPage = () => {
@@ -59,29 +59,106 @@ const PatientShowPage = () => {
         <div>ssn: {ssn}</div>
         <div>occupation: {occupation}</div>
         <div>dateOfBirth: {dateOfBirth}</div>
-        <h3>entries</h3>
-        {entries.map(Entry)}
+        <Card>
+          <Card.Content>
+            <Card.Header>entries</Card.Header>
+          </Card.Content>
+          <Feed>{entries.map(Entry)}</Feed>
+        </Card>
       </Container>
     </div>
   );
 };
 
-const Entry = ({
-  id,
-  date,
-  description,
-  diagnosisCodes = [],
-}: {
+interface EntryProps {
   id: string;
   date: string;
   description: string;
   diagnosisCodes: string[];
-}) => {
+  type: EntryType;
+  discharge?: Discharge;
+  sickLeave?: SickLeave;
+}
+
+const Entry = (props: EntryProps) => {
+  const { type } = props;
+  switch (type) {
+    case EntryType.Hospital:
+      return <HospitalEntry {...props} />;
+    case EntryType.OccupationalHealthcare:
+      return <OccupationalHealthcareEntry {...props} />;
+    default:
+      return <UnknownEntry {...props} />;
+  }
+};
+
+const HospitalEntry = ({
+  id,
+  date,
+  diagnosisCodes,
+  description,
+  discharge,
+}: EntryProps) => {
+  console.log("DISCHARGE", discharge);
   return (
-    <div key={id}>
-      {date} <em>{description}</em>
-      <ul>{diagnosisCodes.map(Diagnose)}</ul>
-    </div>
+    <Feed.Event key={id}>
+      <Feed.Label>
+        <Icon name="stethoscope" />
+      </Feed.Label>
+      <Feed.Content>
+        {diagnosisCodes.map(Diagnose)}
+        <Feed.Date>{date}</Feed.Date>
+        <Feed.Extra text>{description}</Feed.Extra>
+        <Feed.Extra text>
+          <strong>Discharge</strong>
+          {discharge?.criteria}
+          <Feed.Date>{discharge?.date}</Feed.Date>
+        </Feed.Extra>
+      </Feed.Content>
+    </Feed.Event>
+  );
+};
+
+const OccupationalHealthcareEntry = ({
+  id,
+  date,
+  diagnosisCodes,
+  description,
+  sickLeave,
+}: EntryProps) => {
+  return (
+    <Feed.Event key={id}>
+      <Feed.Label>
+        <Icon name="doctor" />
+      </Feed.Label>
+      <Feed.Content>
+        {diagnosisCodes.map(Diagnose)}
+        <Feed.Date>{date}</Feed.Date>
+        <Feed.Extra text>{description}</Feed.Extra>
+        <Feed.Extra text>Start date: {sickLeave?.startDate}</Feed.Extra>
+        <Feed.Extra text>End date: {sickLeave?.endDate}</Feed.Extra>
+      </Feed.Content>
+    </Feed.Event>
+  );
+};
+
+const UnknownEntry = ({
+  id,
+  date,
+  diagnosisCodes,
+  description,
+}: EntryProps) => {
+  return (
+    <Feed.Event key={id}>
+      <Feed.Label>
+        <Icon name="question circle outline" />
+      </Feed.Label>
+      <Feed.Content>
+        {diagnosisCodes.map(Diagnose)}
+        <Feed.Date>{date}</Feed.Date>
+        <Feed.Extra text>{description}</Feed.Extra>
+      </Feed.Content>
+    </Feed.Event>
   );
 };
 
@@ -89,7 +166,7 @@ const Diagnose = (code: string) => {
   const [{ diagnosis }] = useStateValue();
 
   const name = diagnosis[code]?.name;
-  return <li key={code}>{`${code} ${name}`}</li>;
+  return <Feed.Summary key={code}>{name}</Feed.Summary>;
 };
 
 export default PatientShowPage;
